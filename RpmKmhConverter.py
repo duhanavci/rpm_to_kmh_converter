6b27b0cfc04d55c6c1fe7d3eac78fc9fdc0bba3c
duhanavci <2010205044@ogrenci.karabuk.edu.tr>
2023-08-23 17:39:23 +0300

    güncel durum

--- RpmKmhConverter.py
+++ RpmKmhConverter.py
@@ -5,7 +5,7 @@ from PyQt5.uic import loadUi
 class RPMtoKmHConverterApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = loadUi("Converter.ui", self)  
+        self.ui = loadUi("Converter1.ui", self)  
         self.setWindowTitle("RPM to km/h Converter")
 
         self.ui.convert_button.clicked.connect(self.perform_conversion)
@@ -13,23 +13,25 @@ class RPMtoKmHConverterApp(QMainWindow):
         self.ui.lin_input_type.currentIndexChanged.connect(self.update_linear_input)
         self.ui.radius_input_type.currentIndexChanged.connect(self.update_radius_input)
 
-        self.direction_button = QPushButton("Rotational to lineer", self)
-        self.direction_button.setGeometry(10, 10, 180, 30)
+        self.change_convertion_direction_button()
+        self.store_default_values() 
+        self.store_previous_positions()
+
+    def change_convertion_direction_button(self):
         self.direction_button.clicked.connect(self.toggle_conversion_direction)
 
+    def store_default_values(self):
         self.rotation_input_unit = "rpm"
-        self.linear_input_unit = "kmh"
-        self.radius_input_unit = "m"  # Default radius unit
+        self.linear_input_unit = "km/h"
+        self.radius_input_unit = "m"  
         self.rot_to_lin = True
-        self.lin_to_rot_toggle = False  # Toggle for linear to rotational conversion
 
         self.ui.rpm_input.setEnabled(True)
         self.ui.kmh_input.setEnabled(False)  
-        self.ui.lin_input_type.setCurrentIndex(0)  # Set the default index for linear input type
 
-        self.spacing = 20  # Bileşenler arasına ekleyeceğimiz boşluk miktarı
+        self.spacing = 20  
 
-        # Bileşenlerin önceki konumlarını saklamak için değişkenler
+    def store_previous_positions(self):
         self.prev_rpm_input_y = self.ui.rpm_input.y()
         self.prev_kmh_input_y = self.ui.kmh_input.y()
 
@@ -38,7 +40,7 @@ class RPMtoKmHConverterApp(QMainWindow):
 
         self.prev_rot_input_type_y = self.ui.rot_input_type.y()
         self.prev_lin_input_type_y = self.ui.lin_input_type.y()
-
+    
     def changePosition(self):
         #changes input text components position
         self.ui.rpm_input.move(self.ui.rpm_input.x(), self.prev_kmh_input_y)
@@ -52,21 +54,18 @@ class RPMtoKmHConverterApp(QMainWindow):
         self.ui.rot_input_type.move(self.ui.rot_input_type.x(), self.prev_lin_input_type_y)
         self.ui.lin_input_type.move(self.ui.lin_input_type.x(), self.prev_rot_input_type_y)
         self.prev_rot_input_type_y, self.prev_lin_input_type_y = self.prev_lin_input_type_y, self.prev_rot_input_type_y
-        
+
     def toggle_conversion_direction(self):
         self.rot_to_lin = not self.rot_to_lin
         if self.rot_to_lin:
             self.direction_button.setText("rotational to lineer")
             self.ui.rpm_input.setEnabled(True)
             self.ui.kmh_input.setEnabled(False)
-            self.ui.lin_input_type.setEnabled(True)  # Enable linear input type selection
             self.changePosition()
         else:
             self.direction_button.setText("lineer to rotational")
             self.ui.rpm_input.setEnabled(False)
             self.ui.kmh_input.setEnabled(True)
-            self.ui.lin_input_type.setEnabled(False)  # Disable linear input type selection
-            self.lin_to_rot_toggle = True  # Set the toggle to handle linear to rotational conversion
             self.changePosition()
 
     def update_rotation_input(self, index):
@@ -78,43 +77,66 @@ class RPMtoKmHConverterApp(QMainWindow):
     def update_radius_input(self, index):
         self.radius_input_unit = self.ui.radius_input_type.currentText()
 
+    def convert_any_rot_value_type_to_kmh(self, rot_value, radius):            
+        if self.rotation_input_unit == "rad/s":
+            value = rot_value * 9.549296 # 1 rad/s = 9.5492965855137 rpm
+            
+        elif self.rotation_input_unit == "rps":
+            value = rot_value * 60 # 1 rps = 60 rpm
+        else:
+            value = rot_value
+            
+        return value * (60 * 2 * 3.14159 * radius) / 1000
+
+    def convert_to_meters(self, radius):
+        if self.radius_input_unit == "cm":
+            radius /= 100.0  # Convert cm to m
+        elif self.radius_input_unit == "mm":
+            radius /= 1000.0  # Convert mm to m
+        elif self.radius_input_unit == "inches":
+            radius *= 0.0254  # Convert inches to m
+        
+        return radius
+
+    def convert_any_lin_value_type_to_rpm(self, lin_value, radius):    
+        if self.linear_input_unit == "m/s":
+            value = lin_value * 3.6 
+        elif self.linear_input_unit == "mph":
+            value = lin_value * (1/0.621371)
+        else:
+            value = lin_value    
+
+        return value * 1000 / (60 * 2 * 3.14159 * radius)
+    
+    def show_requested_lin_output(self,result):
+        if self.linear_input_unit == "mph":
+            result = result * 0.621371  # km/h to mph
+        elif self.linear_input_unit == "m/s":
+            result = result * 0.277778  # km/h to m/s
+        self.ui.kmh_input.setText("{:.6f}".format(result))
+        
+    def show_requested_rot_output(self,result):
+        if self.rotation_input_unit == "rps":
+            result = result * (1/60)
+        elif self.rotation_input_unit == "rad/s":
+            result = result * (1/9.549296) 
+        self.ui.rpm_input.setText("{:.6f}".format(result))
+
     def perform_conversion(self):
         try:
             value = float(self.ui.rpm_input.text()) if self.rot_to_lin else float(self.ui.kmh_input.text())
             radius = float(self.ui.radius_input.text())
             
-            if self.radius_input_unit == "cm":
-                radius /= 100.0  # Convert cm to m
-            elif self.radius_input_unit == "mm":
-                radius /= 1000.0  # Convert mm to m
-            elif self.radius_input_unit == "inches":
-                radius *= 0.0254  # Convert inches to m
-
+            radius = self.convert_to_meters(radius)
             result = 0
             if self.rot_to_lin:
-                if self.rotation_input_unit == "rpm":
-                    result = value * (60 * 2 * 3.14159 * radius) / 1000
-                elif self.rotation_input_unit == "rad/s":
-                    value = value * 9.549296 # 1 rad/s = 9.5492965855137 rpm
-                    result = value * (60 * 2 * 3.14159 * radius) / 1000
-                elif self.rotation_input_unit == "rps":
-                    value = value * 60
-                    result = value * (60 * 2 * 3.14159 * radius) / 1000
-
-                if self.linear_input_unit == "mph":
-                    result = result * 0.621371  # km/h to mph
-                elif self.linear_input_unit == "m/s":
-                    result = result * 0.277778  # km/h to m/s
-
-                self.ui.kmh_input.setText("{:.6f}".format(result))
-                
-            # ... Diğer dönüşümler aynı kalsın ...
-
-            if self.lin_to_rot_toggle:  # Handle linear to rotational conversion
-                self.lin_to_rot_toggle = False
-                self.ui.kmh_input.setEnabled(False)
-                self.ui.rpm_input.setEnabled(True)
-                self.changePosition()
+                rot_value = value
+                result = self.convert_any_rot_value_type_to_kmh(rot_value, radius)
+                self.show_requested_lin_output(result)
+            else:
+                lin_value = value
+                result = self.convert_any_lin_value_type_to_rpm(lin_value, radius)
+                self.show_requested_rot_output(result)
 
         except ValueError:
             pass
--- RpmKmhConverterDeneme.py
+++ RpmKmhConverterDeneme.py
@@ -5,29 +5,41 @@ from PyQt5.uic import loadUi
 class RPMtoKmHConverterApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = loadUi("Converter.ui", self)  
-        self.setWindowTitle("RPM to km/h Converter")
+        self.ui = loadUi("Converter1.ui", self)  
+        self.setWindowTitle("Converter")
 
         self.ui.convert_button.clicked.connect(self.perform_conversion)
         self.ui.rot_input_type.currentIndexChanged.connect(self.update_rotation_input)
         self.ui.lin_input_type.currentIndexChanged.connect(self.update_linear_input)
         self.ui.radius_input_type.currentIndexChanged.connect(self.update_radius_input)
 
-        self.direction_button = QPushButton("Rotational to lineer", self)
-        self.direction_button.setGeometry(10, 10, 180, 30)
+        self.ui.help_button.clicked.connect(self.show_help_window) # Yardım düğmesine tıklandığında show_help_window metodunu çağır
+
+        self.change_convertion_direction_button()
+        self.store_default_values() 
+        self.store_previous_positions()
+
+
+
+    def show_help_window(self):
+        self.help_window = loadUi("help.ui")  # Yardım penceresini yükle
+        self.help_window.show()  # Yardım penceresini göster
+
+    def change_convertion_direction_button(self):
         self.direction_button.clicked.connect(self.toggle_conversion_direction)
 
+    def store_default_values(self):
         self.rotation_input_unit = "rpm"
         self.linear_input_unit = "km/h"
-        self.radius_input_unit = "m"  # Default radius unit
+        self.radius_input_unit = "m"  
         self.rot_to_lin = True
 
         self.ui.rpm_input.setEnabled(True)
         self.ui.kmh_input.setEnabled(False)  
 
-        self.spacing = 20  # Bileşenler arasına ekleyeceğimiz boşluk miktarı
+        self.spacing = 20  
 
-        # Bileşenlerin önceki konumlarını saklamak için değişkenler
+    def store_previous_positions(self):
         self.prev_rpm_input_y = self.ui.rpm_input.y()
         self.prev_kmh_input_y = self.ui.kmh_input.y()
 
@@ -36,7 +48,7 @@ class RPMtoKmHConverterApp(QMainWindow):
 
         self.prev_rot_input_type_y = self.ui.rot_input_type.y()
         self.prev_lin_input_type_y = self.ui.lin_input_type.y()
-
+    
     def changePosition(self):
         #changes input text components position
         self.ui.rpm_input.move(self.ui.rpm_input.x(), self.prev_kmh_input_y)
@@ -73,52 +85,67 @@ class RPMtoKmHConverterApp(QMainWindow):
     def update_radius_input(self, index):
         self.radius_input_unit = self.ui.radius_input_type.currentText()
 
+    def convert_any_rot_value_type_to_kmh(self, rot_value, radius):            
+        if self.rotation_input_unit == "rad/s":
+            value = rot_value * 9.549296 # 1 rad/s = 9.5492965855137 rpm
+            
+        elif self.rotation_input_unit == "rps":
+            value = rot_value * 60 # 1 rps = 60 rpm
+        else:
+            value = rot_value
+            
+        return value * (60 * 2 * 3.14159 * radius) / 1000
+
+    def convert_to_meters(self, radius):
+        if self.radius_input_unit == "cm":
+            radius /= 100.0  # Convert cm to m
+        elif self.radius_input_unit == "mm":
+            radius /= 1000.0  # Convert mm to m
+        elif self.radius_input_unit == "inches":
+            radius *= 0.0254  # Convert inches to m
+        
+        return radius
+
+    def convert_any_lin_value_type_to_rpm(self, lin_value, radius):    
+        if self.linear_input_unit == "m/s":
+            value = lin_value * 3.6 
+        elif self.linear_input_unit == "mph":
+            value = lin_value * (1/0.621371)
+        else:
+            value = lin_value    
+
+        return value * 1000 / (60 * 2 * 3.14159 * radius)
+    
+    def show_requested_lin_output(self,result):
+        if self.linear_input_unit == "mph":
+            result = result * 0.621371  # km/h to mph
+        elif self.linear_input_unit == "m/s":
+            result = result * 0.277778  # km/h to m/s
+        self.ui.kmh_input.setText("{:.6f}".format(result))
+        
+    def show_requested_rot_output(self,result):
+        if self.rotation_input_unit == "rps":
+            result = result * (1/60)
+        elif self.rotation_input_unit == "rad/s":
+            result = result * (1/9.549296) 
+        self.ui.rpm_input.setText("{:.6f}".format(result))
+
     def perform_conversion(self):
         try:
             value = float(self.ui.rpm_input.text()) if self.rot_to_lin else float(self.ui.kmh_input.text())
             radius = float(self.ui.radius_input.text())
             
-            if self.radius_input_unit == "cm":
-                radius /= 100.0  # Convert cm to m
-            elif self.radius_input_unit == "mm":
-                radius /= 1000.0  # Convert mm to m
-            elif self.radius_input_unit == "inches":
-                radius *= 0.0254  # Convert inches to m
-
+            radius = self.convert_to_meters(radius)
             result = 0
             if self.rot_to_lin:
-                if self.rotation_input_unit == "rpm":
-                    result = value * (60 * 2 * 3.14159 * radius) / 1000
-                elif self.rotation_input_unit == "rad/s":
-                    value = value * 9.549296 # 1 rad/s = 9.5492965855137 rpm
-                    result = value * (60 * 2 * 3.14159 * radius) / 1000
-                elif self.rotation_input_unit == "rps":
-                    value = value * 60
-                    result = value * (60 * 2 * 3.14159 * radius) / 1000
-
-                if self.linear_input_unit == "mph":
-                    result = result * 0.621371  # km/h to mph
-                elif self.linear_input_unit == "m/s":
-                    result = result * 0.277778  # km/h to m/s
-
-                self.ui.kmh_input.setText("{:.6f}".format(result))
+                rot_value = value
+                result = self.convert_any_rot_value_type_to_kmh(rot_value, radius)
+                self.show_requested_lin_output(result)
             else:
-                if self.linear_input_unit == "km/h":
-                    result = value * 1000 / (60 * 2 * 3.14159 * radius)
-                elif self.linear_input_unit == "m/s":
-                    value = value * 3.6 
-                    result = value * 1000 / (60 * 2 * 3.14159 * radius)
-                elif self.linear_input_unit == "mph":
-                    value = value * (1/0.621371)
-                    result = value * 1000 / (60 * 2 * 3.14159 * radius)
+                lin_value = value
+                result = self.convert_any_lin_value_type_to_rpm(lin_value, radius)
+                self.show_requested_rot_output(result)
 
-                if self.rotation_input_unit == "rps":
-                    result = result * (1/60)
-                elif self.rotation_input_unit == "rad/s":
-                    result = result * (1/9.549296)  
-
-                #result = value * 1000 / (60 * 2 * 3.14159 * radius)
-                self.ui.rpm_input.setText("{:.6f}".format(result))
         except ValueError:
             pass
 
